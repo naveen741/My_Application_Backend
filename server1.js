fdd98b56252caf24617abce25fe6b55afe6b8968
Naveen Kumar A <naveenkumar.a@tringapps.com>
2022-06-09 16:18:07 +0530

    Added a Procfile.

--- server1.js
+++ server1.js
@@ -72,4 +72,87 @@ app.post('/checkdata',(req,res)=>{
     res.status(200).send({ status: 200, message: flag.message })
   }) 
 });
-
+app.post(`/onlineCompiler`,(req,res)=>{
+  const dir = 'temp';
+  if (!fs.existsSync(dir)){
+      fs.mkdirSync(dir);
+    }
+  console.log(req.body)
+  const codeDetail=req.body
+  
+  function addData(data){
+      const mergeData=(datas)=>{
+          crtOutput=datas.output; 
+          data={...data,crtOutput};
+          console.log(data);
+          sendData(data)
+      };
+      let crtOutput=null;
+      const envData={OS : "Linux"};
+      if(codeDetail.inputRadio)
+          compiler.compileJavaWithInput(envData , codeDetail.crtCode, codeDetail.input , (datas)=>{mergeData(datas)});     	
+      else
+          compiler.compileJava(envData , codeDetail.code , addData);
+      
+      //console.log(data)
+  }
+  if(codeDetail.lang === 'C'||codeDetail.lang === 'C++'){
+    const envData =  { OS : "Linux" , cmd : 'gcc'}
+    if(codeDetail.inputRadio)
+      compiler.compileCPPWithInput(envData , codeDetail.code, codeDetail.input , addData);     	
+    else
+      compiler.compileCPP(envData , codeDetail.code , addData);
+  }
+  else if(codeDetail.lang === 'Python'){
+    const envData={OS : "Linux"};
+    if(codeDetail.inputRadio)
+      compiler.compilePythonWithInput(envData , codeDetail.code, codeDetail.input , addData);     	
+    else
+      compiler.compilePython(envData , codeDetail.code , addData);
+  }
+  else if(codeDetail.lang === 'Java')
+  {
+    const envData={OS : "Linux"};
+    if(codeDetail.inputRadio)
+      compiler.compileJavaWithInput(envData , codeDetail.code, codeDetail.input , addData);     	
+    else
+      compiler.compileJava(envData , codeDetail.code , addData);
+  }
+  
+  function sendData(data) {     
+      if (fs.existsSync(dir)){ 
+      fs.rmdir(dir, { recursive: true }, (err) => {
+          if (err) {
+              throw err;
+          }
+      }); 
+      }
+      if (!fs.existsSync(dir))
+      {
+      fs.mkdirSync(dir);
+      }
+    if(data.error)
+    {
+      try{
+        res.send({output : data.error})
+        res.end()
+      }
+      catch(e){
+        console.log(e);
+      }
+              
+    }
+    else
+    {
+      //console.log(data.output)
+      try{
+          //console.log(data)
+          res.send(data)
+          res.end()
+      }
+      catch(e){
+        console.log(e);
+      }
+    }
+  }
+})
